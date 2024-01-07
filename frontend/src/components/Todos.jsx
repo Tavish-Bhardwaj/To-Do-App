f7c39048646866dde810b88d2eb490f40f5257fe
Tavish Bhardwaj <bhardwajtavishofficial@gmail.com>
2024-01-08 01:55:19 +0530

    some frontend files added.

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,7 +1,6 @@
-export function Todos(){
+export function Todos({todos}){
+
     return <div>
-        <h1>Go to  Gym</h1>
-        <h2>You need to go to the Gym</h2>
-        <button>Mark as Completed</button>
+    
     </div>
 }
